88a8fcfad182f65df5387e192f484d3c300c65b0
Damien VIE <damien.vie@me.com>
2023-03-29 23:02:40 +0200

    debug code fbx importer

--- framework_unreal/core/asset.py
+++ framework_unreal/core/asset.py
@@ -1,4 +1,4 @@
-from typing import List, Type, Optional
+from typing import Type, Optional, Union
 import unreal
 import re
 
@@ -20,7 +20,7 @@ class BaseAsset:
     def __init__(
         self, asset_path: str, asset_name: str, asset_type: Type[unreal.Object]
     ):
-        """Initializes a new instance of the BaseAsset class.
+        """Initialize a new instance of the BaseAsset class.
 
         Args:
             asset_path (str): The path where to create the asset.
@@ -32,7 +32,7 @@ class BaseAsset:
         self.asset_name: str = self._get_asset_name(asset_name)
 
     def attribute_name_template(self) -> str:
-        """Gets the attribute name template.
+        """Get the attribute name template.
 
         Raises:
             NotImplementedError: If the method is not implemented in the derived class.
@@ -56,14 +56,15 @@ class BaseAsset:
         pattern = self.attribute_name_template().format(r"\w+")
         return bool(re.match(pattern, name))
 
-    def check_asset_exists(asset_path: str) -> Optional[bool]:
+    def check_asset_exists(self, asset_path: str) -> Optional[bool]:
         """Check if an asset exists in Unreal Engine.
 
         Args:
             asset_path (str): The path to the asset to check.
 
         Returns:
-            Optional[bool]: True if the asset exists, False if it does not exist, and None if an error occurred.
+            Optional[bool]: True if the asset exists, False if it does not exist,
+            and None if an error occurred.
         """
         try:
             return unreal.EditorAssetLibrary.does_asset_exist(asset_path)
@@ -72,7 +73,7 @@ class BaseAsset:
             return None
 
     def _get_asset_name(self, asset_name: str, key_name: str = "default") -> str:
-        """Gets the asset name.
+        """Get the asset name.
 
         Args:
             asset_name (str): The base name of the asset.
@@ -84,7 +85,7 @@ class BaseAsset:
         return self.attribute_name_template().format(asset_name)
 
     def _get_creation_options(self) -> None:
-        """Defines the asset creation options.
+        """Define the asset creation options.
 
         Returns:
             obj: The asset creation options.
@@ -94,13 +95,12 @@ class BaseAsset:
         )
 
     def create_asset(self) -> unreal.Object:
-        """Creates the asset.
+        """Create the asset.
 
         Returns:
             unreal.Object: The created asset object.
         """
-
-        if self.check_asset_exists():
+        if self.check_asset_exists(self.asset_path):
             unreal.log_warning(
                 "The asset {} already exists in the path {}.".format(
                     self.asset_name, self.asset_path
@@ -122,7 +122,7 @@ class BaseAsset:
             return asset
 
     def save_asset(self, asset: unreal.Object) -> None:
-        """Saves the asset.
+        """Save the asset.
 
         Args:
             asset (unreal.Object): The asset object to save.
@@ -136,18 +136,18 @@ class FBXImporter:
     A class for importing FBX files into Unreal Engine 4.
 
     Attributes:
-    editor_util (unreal.EditorUtilityLibrary): A utility library for working with the Editor.
     asset_tools (unreal.AssetTools): A set of helper functions for creating and manipulating assets.
 
     Methods:
     import_fbx(fbx_path: str, destination_path: str, asset_name: str, import_animation: bool = False,
                 animation_sequence_name: Optional[str] = None) -> unreal.Asset: Imports the FBX file
                 located at 'fbx_path' into UE4 at the specified 'destination_path' and 'asset_name'.
-    """
+    """  # noqa
 
     def __init__(self):
-        self.editor_util: unreal.EditorUtilityLibrary = unreal.EditorUtilityLibrary()
+        """Initialize a new instance of the FBXImporter class."""
         self.asset_tools: unreal.AssetTools = unreal.AssetToolsHelpers.get_asset_tools()
+        self.asset_reg = unreal.AssetRegistryHelpers.get_asset_registry()
 
     def import_fbx(
         self,
@@ -160,9 +160,8 @@ class FBXImporter:
         import_textures: bool = True,
         import_mesh: bool = True,
         replace_existing: bool = True,
-    ) -> unreal.Asset:
-        """
-        Imports an FBX file into UE.
+    ) -> Union[unreal.SkeletalMesh, unreal.StaticMesh]:
+        """Import an FBX file into UE.
 
         Args:
         fbx_path (str): The path to the FBX file to be imported.
@@ -176,50 +175,49 @@ class FBXImporter:
         replace_existing (bool, optional): Whether to replace an existing asset with the same name as the imported asset.
 
         Returns:
-        unreal.Asset: The imported asset.
-        """
+        Union[unreal.skeletalMesh, unreal.staticMesh]: The imported asset.
+        """  # noqa
         import_task: unreal.AssetImportTask = unreal.AssetImportTask()
-        import_task.set_editor_property("filename", fbx_path)
-        import_task.set_editor_property("destination_path", destination_path)
-        import_task.set_editor_property("destination_name", asset_name)
-        import_task.set_editor_property("replace_existing", replace_existing)
+        import_task.filename = fbx_path
+        import_task.destination_path = destination_path
+        import_task.destination_name = asset_name
+        import_task.replace_existing = replace_existing
 
         options: unreal.FbxImportUI = unreal.FbxImportUI()
-        options.set_editor_property("import_mesh", import_mesh)
-        options.set_editor_property("import_materials", import_materials)
-        options.set_editor_property("import_textures", import_textures)
-        options.set_editor_property("import_animations", import_animation)
+        options.import_mesh = import_mesh
+        options.import_materials = import_materials
+        options.import_textures = import_textures
+        options.import_animations = import_animation
 
-        import_task.set_editor_property("options", options)
+        import_task.options = options
 
         self.asset_tools.import_asset_tasks([import_task])
 
         asset_path: str = destination_path + asset_name
-        asset: unreal.Asset = unreal.load_asset(asset_path)
+        asset: unreal.Object = unreal.load_asset(asset_path)
 
-        if isinstance(asset, unreal.StaticMesh):
-            self.editor_util.set_actor_label("SM_" + asset.name, asset_name)
-
+        if isinstance(asset, unreal.SkeletalMesh):
+            asset.rename("SKM_" + asset_name)
             # Rename physics asset if it exists
             physics_asset: unreal.PhysicsAsset = asset.get_editor_property(
                 "physics_asset"
             )
             if physics_asset:
-                self.editor_util.rename_asset(physics_asset, "PA_" + asset_name)
-
+                physics_asset.rename("PA_" + asset_name)
             # Rename skeleton asset if it exists
             skeleton: unreal.Skeleton = asset.get_editor_property("skeleton")
             if skeleton:
-                self.editor_util.rename_asset(skeleton, "SK_" + asset_name)
-
-        elif isinstance(asset, unreal.SkeletalMesh):
-            self.editor_util.set_actor_label("SKM_" + asset.name, asset_name)
+                skeleton.rename("SKL_" + asset_name)
+        elif isinstance(asset, unreal.StaticMesh):
+            asset.rename("SM_" + asset_name)
 
         if import_animation and animation_sequence_name:
-            animations: List[unreal.AnimationAsset] = unreal.find_assets(
-                asset_path, asset_class=unreal.AnimationAsset
-            )
-            for anim in animations:
-                self.editor_util.rename_asset(anim, animation_sequence_name)
+            assets_in_package = self.asset_reg.get_assets_by_path(destination_path)
+            for asset_in_package in assets_in_package:
+                if asset_in_package.asset_class == unreal.AnimationAsset:
+                    name = asset_in_package.asset_name()
+                    if name.startswith("AS_"):
+                        continue
+                    asset_in_package.rename("AS_" + asset_name + name)
 
         return asset
